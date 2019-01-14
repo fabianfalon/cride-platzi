aaab3a6dd552203d300c7e4271c41a216e9fc291
Pablo Trinidad <pabloatp0204@gmail.com>
2019-01-14 11:29:36 -0500

    Proxy models

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -32,3 +32,25 @@ class CRideModel(models.Model):
 
         get_latest_by = 'created'
         ordering = ['-created', '-modified']
+
+
+
+class Person(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def say_hi(name):
+        pass
+
+
+MyPerson.objects.all()
+ricardo = MyPerson.objects.get(pk=1)
+ricardo.say_hi('Pablo')
+
+rulo = Person.objects.get(pk=2)
+rulo.say_hi('Pablo')
