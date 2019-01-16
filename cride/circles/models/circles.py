68a57213aaf95c9ffb7fd8f33f9cd9ebe3a7321c
Pablo Trinidad <pabloatp0204@gmail.com>
2019-01-16 10:06:11 -0500

    Membership circle

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -21,7 +21,13 @@ class Circle(CRideModel):
     about = models.CharField('circle description', max_length=255)
     picture = models.ImageField(upload_to='circles/pictures', blank=True, null=True)
 
-    # Stats
+    members = models.ManyToManyField(
+        'users.User',
+        through='circles.Membership',
+        through_fields=('circle', 'user')
+    )
+
+    # Stats
     rides_offered = models.PositiveIntegerField(default=0)
     rides_taken = models.PositiveIntegerField(default=0)
 
