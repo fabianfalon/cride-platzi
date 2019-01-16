2398790347ffbd8abfa80fb068ddbd7eaa782694
Pablo Trinidad <pabloatp0204@gmail.com>
2019-01-16 08:42:50 -0500

    Login limited to verified accounts

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -4,12 +4,12 @@
 from django.contrib.auth import password_validation, authenticate
 from django.core.validators import RegexValidator
 
-# Django REST Framework
+# Django REST Framework
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
 
-# Models
+# Models
 from cride.users.models import User, Profile
 
 
@@ -44,18 +44,18 @@ class UserSignUpSerializer(serializers.Serializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
-    # Phone number
+    # Phone number
     phone_regex = RegexValidator(
         regex=r'\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: +999999999. Up to 15 digits allowed."
     )
     phone_number = serializers.CharField(validators=[phone_regex])
 
-    # Password
+    # Password
     password = serializers.CharField(min_length=8, max_length=64)
     password_confirmation = serializers.CharField(min_length=8, max_length=64)
 
-    # Name
+    # Name
     first_name = serializers.CharField(min_length=2, max_length=30)
     last_name = serializers.CharField(min_length=2, max_length=30)
 
@@ -71,8 +71,8 @@ class UserSignUpSerializer(serializers.Serializer):
     def create(self, data):
         """Handle user and profile creation."""
         data.pop('password_confirmation')
-        user = User.objects.create_user(**data)
-        profile = Profile.objects.create(user=user)
+        user = User.objects.create_user(**data, is_verified=False)
+        Profile.objects.create(user=user)
         return user
 
 
@@ -90,6 +90,8 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Invalid credentials')
+        if not user.is_verified:
+            raise serializers.ValidationError('Account is not active yet :(')
         self.context['user'] = user
         return data
 
