ff7c27e679c9598e21de0661952f8f3fd42867dc
Pablo Trinidad <pabloatp0204@gmail.com>
2019-01-18 10:39:00 -0500

    Admin actions: updating query

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -25,3 +25,15 @@ class CircleAdmin(admin.ModelAdmin):
         'verified',
         'is_limited'
     )
+
+    actions = ['make_verified', 'make_unverified']
+
+    def make_verified(self, request, queryset):
+        """Make circles verified."""
+        queryset.update(verified=True)
+    make_verified.short_description = 'Make selected circles verified'
+
+    def make_unverified(self, request, queryset):
+        """Make circles unverified."""
+        queryset.update(verified=False)
+    make_unverified.short_description = 'Make selected circles unverified'
